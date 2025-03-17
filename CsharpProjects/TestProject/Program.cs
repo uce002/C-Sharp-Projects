6ff1f2b616738fee7e7608e32e07a650c4e3ec4e
Uzair Yousaf <uzairyousaf1@icloud.com>
2025-03-17 12:05:06 +0000

    nested the double and triple if statements so both bonuses aren't awarded. changed the win/lose functionality to several prizes based on score

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -18,24 +18,36 @@ int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
 
+roll1 = 6;
+roll2 = 6;
+roll3 = 6;
+
 int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
+
 if(roll1 == roll2 || roll2 == roll3 || roll3 == roll1) {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-}
 
-if((roll1 == roll2) && (roll2 == roll3)) {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    if((roll1 == roll2) && (roll2 == roll3)) {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    } else {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;   
+    }
 }
+    
 
-if(total >= 15) {
-    Console.WriteLine("You win!");
-}
-
-if(total < 15) {
-    Console.WriteLine("Sorry, you lose.");
+if(total >= 16) {
+    Console.WriteLine("You win a new car!");
+} 
+else if(total >= 10) {
+    Console.WriteLine("You win a new laptop!");
+} 
+else if (total == 7) {
+    Console.WriteLine("You win a trip for two!");
+} 
+else {
+    Console.WriteLine("You win a kitten!");
 }
