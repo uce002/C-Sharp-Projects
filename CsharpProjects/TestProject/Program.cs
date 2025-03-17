bd161f39be1264c224f14817ff21e19e40c450a0
Uzair Yousaf <uzairyousaf1@icloud.com>
2025-03-17 11:28:42 +0000

    These are the projects I am making as part of the Microsoft C# certification

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿// using System.ComponentModel.DataAnnotations;
+
+// Random dice = new Random(); //new instance of a class (object). to access 
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue;
+
+// largerValue = Math.Max(firstValue, secondValue);
+
+// Console.WriteLine(largerValue);
+
